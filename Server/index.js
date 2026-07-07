91eb96b84e4602cbb56d25077875f24761f31d0f
Vicky Kumar <vickykumarpathaldiha7@gmail.com>
2026-07-07 20:50:41 +0530

    project setup

---
+++ Server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app=express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT=process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
